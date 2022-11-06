e4e741ae764efc4a6a4a1d7f39ec1a61c61ca135
seregskupow <sergskypow@gmail.com>
2022-11-06 02:00:17 +0200

    Setup google cloud deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t seregask/multi-client-k8s:latest -t seregask/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t seregask/multi-server-k8s:latest -t seregask/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+docker build -t seregask/multi-worker-k8s:latest -t seregask/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+
+docker push seregask/multi-client-k8s:latest
+docker push seregask/multi-server-k8s:latest
+docker push seregask/multi-worker-k8s:latest
+
+docker push seregask/multi-client-k8s:$SHA
+docker push seregask/multi-server-k8s:$SHA
+docker push seregask/multi-worker-k8s:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=seregask/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=seregask/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=seregask/multi-worker-k8s:$SHA
